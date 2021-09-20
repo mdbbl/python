a8b68392601758f8b89d79bce753033eeffdaf8c
Md Hossain <52868310+mdbbl@users.noreply.github.com>
2021-09-20 11:54:32 -0400

    Add files via upload

---
+++ test second.py
@@ -0,0 +1,31 @@
+print('learning python')
+print('using python idl')
+print('I am learning python.it is my first experience. it is so interenting to me')
+code('Python 3.7.9 (bundled)
+>>> 4+4
+8
+>>> %Run test.py
+learning python
+using python idl
+I am learning python.it is my first experience. it is so interenting to me
+>>> %Run helloworld.py
+hello world
+how are you today
+>>> test.py+helloworld.py
+Traceback (most recent call last):
+  File "<pyshell>", line 1, in <module>
+NameError: name 'test' is not defined
+>>> %Run helloworld.py
+hello world
+how are you today
+>>> %Run 'test second.py'
+learning python
+Traceback (most recent call last):
+  File "C:\Users\User\Desktop\python\test second.py", line 2, in <module>
+    pront('using python idl')
+NameError: name 'pront' is not defined
+>>> %Run 'test second.py'
+learning python
+using python idl
+I am learning python.it is my first experience. it is so interenting to me
+>>> ')
---
+++ test.py
@@ -0,0 +1,3 @@
+print('learning python')
+print('using python idl')
+print('I am learning python.it is my first experience. it is so interenting to me')
